ca238f1c07052d49c30e726c8f6bfb5e1ca37008
Robbie Pallas <robbie.pallas@piksel.com>
2017-03-05 01:12:12 +0000

    async await implementation

--- async-await.js
+++ async-await.js
@@ -1,10 +1,146 @@
-
-async doSomethingAsync () {
-  
-  // Run some stuff in parallel that we need up front
-  const [one, two, three] = await Promise.all([parallelOne(), parallelTwo(), parallelThree()]);
-
-  // Run some things async but in sequence with the above values
-  const four = await asyncNeedingOne(one);
-  const five = await asyncNeedingTwoAndThree(two, three);
+function waitAndLog(msg) {
+  return new Promise(resolve => setTimeout(function () {
+    console.log(msg, 'Done');
+    resolve(`${msg} data`);
+  }, 1000));
 }
+
+// With the functions inline (with logging)
+
+async function run () {
+  try {
+    const a = await async function A () {
+      console.log('calling A');
+      return await waitAndLog('A');
+    }();
+    const [b, c] = await Promise.all([
+      async function B (...args) {
+        console.log('calling B with', args);
+        return await waitAndLog('B');
+      }(a),
+      async function C (...args) {
+        console.log('calling C with', args);
+        return await waitAndLog('C');
+      }(a)
+    ]);
+    const d = await async function D (...args) {
+      console.log('calling D with', args);
+      return await waitAndLog('D');
+    }(a, b);
+    const e = await async function E (...args) {
+      console.log('calling E with', args);
+      return await waitAndLog('E');
+    }(a, c);
+    console.log('final results: ', { A: a, B: b, C: c, D: d, E: e });
+  } catch (err) {
+    console.log('err: ', err);
+  }
+}
+
+run();
+
+// With the functions inline (no logging)
+
+// async function run () {
+//   try {
+//     const a = await async function A () {
+//       return await waitAndLog('A');
+//     }();
+//     const [b, c] = await Promise.all([
+//       async function B (...args) {
+//         return await waitAndLog('B');
+//       }(a),
+//       async function C (...args) {
+//         return await waitAndLog('C');
+//       }(a)
+//     ]);
+//     const d = await async function D (...args) {
+//       return await waitAndLog('D');
+//     }(a, b);
+//     const e = await async function E (...args) {
+//       return await waitAndLog('E');
+//     }(a, c);
+//     console.log('final results: ', { A: a, B: b, C: c, D: d, E: e });
+//   } catch (err) {
+//     console.log('err: ', err);
+//   }
+// }
+//
+// run();
+
+// With the functions extracted (with logging)
+
+// async function A () {
+//   console.log('calling A');
+//   return await waitAndLog('A');
+// }
+//
+// async function B (...args) {
+//   console.log('calling B with', args);
+//   return await waitAndLog('B');
+// }
+//
+// async function C (...args) {
+//   console.log('calling C with', args);
+//   return await waitAndLog('C');
+// }
+//
+// async function D (...args) {
+//   console.log('calling D with', args);
+//   return await waitAndLog('D');
+// }
+//
+// async function E (...args) {
+//   console.log('calling E with', args);
+//   return await waitAndLog('E');
+// }
+//
+// async function run () {
+//   try {
+//     const a = await A();
+//     const [b, c] = await Promise.all([B(a), C(a)]);
+//     const d = await D(a, b);
+//     const e = await E(a, c);
+//     console.log('final results: ', { A: a, B: b, C: c, D: d, E: e });
+//   } catch (err) {
+//     console.log('err: ', err);
+//   }
+// }
+//
+// run();
+
+// With the functions extracted (with logging)
+
+// async function A () {
+//   return await waitAndLog('A');
+// }
+//
+// async function B (...args) {
+//   return await waitAndLog('B');
+// }
+//
+// async function C (...args) {
+//   return await waitAndLog('C');
+// }
+//
+// async function D (...args) {
+//   return await waitAndLog('D');
+// }
+//
+// async function E (...args) {
+//   return await waitAndLog('E');
+// }
+//
+// async function run () {
+//   try {
+//     const a = await A();
+//     const [b, c] = await Promise.all([B(a), C(a)]);
+//     const d = await D(a, b);
+//     const e = await E(a, c);
+//     console.log('final results: ', { A: a, B: b, C: c, D: d, E: e });
+//   } catch (err) {
+//     console.log('err: ', err);
+//   }
+// }
+//
+// run();
--- async.js
+++ async.js
@@ -7,6 +7,9 @@ const waitAndLog = (msg, callback) => {
   }, 1000);
 };
 
+
+// With the functions inline (with logging)
+
 Async.auto({
   A: (callback) => {
     console.log('calling A');
@@ -31,3 +34,116 @@ Async.auto({
 }, (err, results) => {
   console.log(err || 'final results: ', results);
 });
+
+// With the functions inline (no logging)
+
+// Async.auto({
+//   A: (callback) => {
+//     waitAndLog('A', callback);
+//   },
+//   B: ['A', (results, callback) => {
+//     waitAndLog('B', callback);
+//   }],
+//   C: ['A', (results, callback) => {
+//     waitAndLog('C', callback);
+//   }],
+//   D: ['A', 'B', (results, callback) => {
+//     waitAndLog('D', callback);
+//   }],
+//   E: ['A', 'C', (results, callback) => {
+//     waitAndLog('E', callback);
+//   }]
+// }, (err, results) => {
+//   console.log(err || 'final results: ', results);
+// });
+
+
+// With the functions extracted (with logging)
+
+// function A () {
+//   return (callback) => {
+//     console.log('calling A');
+//     waitAndLog('A', callback);
+//   }
+// }
+//
+// function B () {
+//   return (results, callback) => {
+//     console.log('calling B with:', results);
+//     waitAndLog('B', callback);
+//   }
+// }
+//
+// function C () {
+//   return (results, callback) => {
+//     console.log('calling C with:', results);
+//     waitAndLog('C', callback);
+//   }
+// }
+//
+// function D () {
+//   return (results, callback) => {
+//     console.log('calling D with:', results);
+//     waitAndLog('D', callback);
+//   }
+// }
+//
+// function E () {
+//   return (results, callback) => {
+//     console.log('calling E with:', results);
+//     waitAndLog('E', callback);
+//   }
+// }
+//
+// Async.auto({
+//   A: A(),
+//   B: ['A', B()],
+//   C: ['A', C()],
+//   D: ['A', 'B', D()],
+//   E: ['A', 'C', E()]
+// }, (err, results) => {
+//   console.log(err || 'final results: ', results);
+// });
+
+
+// With the functions extracted (no logging)
+
+// function A () {
+//   return (callback) => {
+//     waitAndLog('A', callback);
+//   }
+// }
+//
+// function B () {
+//   return (results, callback) => {
+//     waitAndLog('B', callback);
+//   }
+// }
+//
+// function C () {
+//   return (results, callback) => {
+//     waitAndLog('C', callback);
+//   }
+// }
+//
+// function D () {
+//   return (results, callback) => {
+//     waitAndLog('D', callback);
+//   }
+// }
+//
+// function E () {
+//   return (results, callback) => {
+//     waitAndLog('E', callback);
+//   }
+// }
+//
+// Async.auto({
+//   A: A(),
+//   B: ['A', B()],
+//   C: ['A', C()],
+//   D: ['A', 'B', D()],
+//   E: ['A', 'C', E()]
+// }, (err, results) => {
+//   console.log(err || 'final results: ', results);
+// });
